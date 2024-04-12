4b488cdcaf28c3e7eed087f5e3065f5a86444ad7
Gideon Mutai <giddymutai13@gmail.com>
2024-04-13 00:32:02 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+// removing the element with id of 'main'
+const main = document.getElementById("main");
+main.remove();
+
+// creating a new element
+const newHeader = document.createElement("h1");
+document.body.append(newHeader);
+
+// adding an ID to the newHeader variable
+newHeader.id = "victory";
+
+// Adding text to the newHeader variable
+newHeader.innerHTML = "Gideon is the champion";
+
